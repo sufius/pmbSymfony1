4c14093b3061c3712980119dafcc5d98539fbdd4
Sufian Abu-Rab <s.aburab@gmail.com>
2016-05-13 17:48:56 +0200

    revert: make the method ->generate() working for _dev.php urls too (used in the layout to generated the language links in the language dropdown). solution: had to pass the variables without to be distracted by the sfOutputDecorator

--- lib/routing/sfRoute.class.php
+++ lib/routing/sfRoute.class.php
@@ -249,8 +249,6 @@ class sfRoute implements Serializable
 
     if ($this->options['extra_parameters_as_query_string'] && !$this->hasStarParameter())
     {
-      // make this working for _dev URLs too
-      $params = is_array($params) ? $params : array($params);
       // add a query string if needed
       if ($extra = array_diff_key($params, $this->variables, $defaults))
       {
